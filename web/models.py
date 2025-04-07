743c8bf2439c4faefe14a8859f2ae44aaeaee0e6
oriolvines <oriolvibo04@gmail.com>
2025-04-07 12:03:49 +0200

    new models functioning

--- web/models.py
+++ web/models.py
@@ -12,10 +12,6 @@ class Album(models.Model):
     def __str__(self):
         return self.title
 
-class ListeningHistory(models.Model):
-    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='listening_history')
-    track = models.ForeignKey(Track, on_delete=models.CASCADE)
-    played_at = models.DateTimeField()
 
 class Track(models.Model):
     title = models.CharField(max_length=100)
@@ -26,13 +22,11 @@ class Track(models.Model):
     def __str__(self):
         return self.title
 
-class SpotifyUser(models.Model):
-    spotify_id = models.CharField(max_length=50, unique=True)
-    display_name = models.CharField(max_length=100)
-    profile_image = models.URLField(null=True, blank=True)
-    def __str__(self):
-        return self.display_name
 
+class ListeningHistory(models.Model):
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='listening_history')
+    track = models.ForeignKey(Track, on_delete=models.CASCADE)
+    played_at = models.DateTimeField()
 
 # For more info on how to use Django model fields: 
 # https://docs.djangoproject.com/en/stable/ref/models/fields/
