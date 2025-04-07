32af714e20abf5d25382f0960ac08a279f847865
oriolvines <oriolvibo04@gmail.com>
2025-04-07 12:08:45 +0200

    admin.py

--- web/admin.py
+++ web/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Artist
+from .models import Artist, ListeningHistory, Track, Album
 
 admin.site.register(Artist)
+admin.site.register(Track)
+admin.site.register(ListeningHistory)
+admin.site.register(Album)
