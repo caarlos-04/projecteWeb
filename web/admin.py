3576afca25e8c290f49212035bbaaa03d6781176
blaigene <genemorablai@gmail.com>
2025-05-15 12:38:42 +0200

    Playlists added to Admin Personalized

--- web/admin.py
+++ web/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from .models import Artist, Track, Album, Playlist
 
+
+# Admin bàsic per Artist, Track i Album
 admin.site.register(Artist)
 admin.site.register(Track)
 admin.site.register(Album)
-admin.site.register(Playlist)
+
+# Admin personalitzat per Playlist
+@admin.register(Playlist)
+class PlaylistAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user')
+    filter_horizontal = ('tracks',)
