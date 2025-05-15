dd7d50f014c0c8d8ff2ff6bd07009b0cbba8aa6b
blaigene <genemorablai@gmail.com>
2025-05-15 12:22:45 +0200

    Playlists added to Admin

--- web/admin.py
+++ web/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Artist, Track, Album
+from .models import Artist, Track, Album, Playlist
 
 admin.site.register(Artist)
 admin.site.register(Track)
 admin.site.register(Album)
+admin.site.register(Playlist)
